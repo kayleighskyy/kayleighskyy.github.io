5ee363b49f63916cc69e8e190b4b1dbac7064840
kayleighskyy <kayleighskyy03@gmail.com>
2025-12-03 21:19:42 -0600

    Update Homework3.js

--- Homework4/Homework3.js
+++ Homework4/Homework3.js
@@ -318,5 +318,15 @@ document.addEventListener('DOMContentLoaded', function() {
         sel.addEventListener('change', updateSubmitButton);
     });
     updateSubmitButton();
+
+const select = document.getElementById('state');
+const response = await fetch("https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json");
+const statesObj = await response.json();
+    for (const [code, name] of Object.entries(states.obj)) {
+        const opt = document.createElement("option");
+        opt.value = code;
+        opt.textContent = name;
+        select.sppendChild(opt);
+    }
                                                  
 });
