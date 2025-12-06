68fa9535f87d018eec620f72e95b985bad142b5a
kayleighskyy <kayleighskyy03@gmail.com>
2025-12-05 19:35:50 -0600

    Update Homework3.js

--- Homework4/Homework3.js
+++ Homework4/Homework3.js
@@ -321,14 +321,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function setCookie (cname, cvalue, exdays) {
     const d = new Date();
-    d.setTime(d.getTime() + exdays*24*60*60*1000));
-    let expires = "expires=" + cvalue + ";" + expires + ";path=/";
+    d.setTime(d.getTime() + exdays*24*60*60*1000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
     let name = cname + "="; 
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';'); 
+    let ca = document.cookie.split(';'); 
     for(let i = 0; i < ca.length; i++)
 {
     let c = ca[i];
